f6b2d45926f7776a6a447db98ee5b8e1bf2ebe5c
ROZ a.k.a. Aoi Emerauda <mail@mofumofu.me>
2024-04-25 02:52:13 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
-# nodemap
-Node Map
+# Node Map
+
+This project provides a web application for displaying information about cryptocurrensy coin daemon nodes. It visualizes node data on a Google Map and displays detailed information in a table format. The project is built with Node.js and uses PureCSS for styling.
+
+## Features
+
+- Visual representation of coin nodes on a Google Map.
+- Detailed table view showing:
+  - IP address
+  - User agent
+  - Block height
+  - Country
+  - Location (City)
+  - Network details
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+- You have installed Node.js and npm.
+- You have a basic understanding of JavaScript and Node.js.
+- You have a IPinfo.io token or something else.
+- You have a Google Maps API key.
+- You have a Reverse Proxy. (Recommend Nginx) 
+
+## Installing Node Map
+
+To install Node Map, follow these steps:
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/ROZ-MOFUMOFU-ME/nodemap.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd nodemap
+   ```
+3. Install the necessary packages:
+   ```bash
+   npm install
+   ```
+4. Add your Google Maps API key to the `index.html` file:
+   ```html
+   <script src="https://maps.googleapis.com/maps/api/js?key=<YOUR_GOOGLEMAPS_API_KEY&callback=initMap" async defer></script>
+   ```
+
+## Using Node Map
+
+To use Node Map, run the following command from the root of the project:
+
+```bash
+node app.js
+```
+
+or
+
+```bash
+npm start
+```
+
+or
+
+```bash
+pm2 start app.js --name nodemap
+```
+
+Open your web browser and navigate to `http://localhost:3000` to view the application.
+
+## Contributing to Node Map
+
+To contribute to Node Map, follow these steps:
+
+1. Fork this repository.
+2. Create a branch: `git checkout -b <branch_name>`.
+3. Make your changes and commit them: `git commit -m '<commit_message>'`
+4. Push to the original branch: `git push origin <project_name>/<location>`
+5. Create the pull request.
+
+Alternatively, see the GitHub documentation on [creating a pull request](https://help.github.com/articles/creating-a-pull-request/).
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details.
+
+## Contact
+
+ROZ: [@ROZ_mofumofu_me](https://twitter.com/ROZ_mofumofu_me)
+
+Aoi Emerauda: [@Aoi_Emerauda](https://twitter.com/Aoi_Emerauda) Alternative
+
+Project Link: [https://github.com/ROZ-MOFUMOFU-ME/ping](https://github.com/ROZ-MOFUMOFU-ME/nodemap)
+
+## Donations
+
+Donations for development are greatly appreciated!
+
+[GitHub Sponsors](https://github.com/sponsors/ROZ-MOFUMOFU-ME)
+
+[Patreon](https://patreon.com/emerauda)
+
+[FANBOX](https://emerauda.fanbox.cc/)
+
+[Fantia](https://fantia.jp/emerauda)
+
+[Buy Me a Coffee](https://buymeacoffee.com/emerauda)
+
+BTC: 3C8oCWjVs2sycQcK3ttiPRSKV4AKBhC7xT
+
+ETH: 0xc664a0416c23b1b13a18e86cb5fdd1007be375ae
+
+LTC: Lh96WZ7Rw9Wf4GDX2KXpzieneZFV5Xe5ou
+
+BCH: pzdsppue8uwc20x35psaqq8sgchkenr49c0qxzazxu
+
+ETC: 0xc664a0416c23b1b13a18e86cb5fdd1007be375ae
+
+MONA: MLEqE3vi11j4ZguMjkvMn5rUtze6kXbAzQ
---
+++ app.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const Client = require('bitcoin-core');
+const axios = require('axios');
+const app = express();
+
+const client = new Client({
+    host: '127.0.0.1',
+    port: 8333,
+    username: 'USERNAME',
+    password: 'PASSWORD',
+    ssl: false,
+    timeout: 30000
+});
+
+const ipInfoToken = 'YOUR_IPINFO_TOKEN';
+
+client.command('getpeerinfo').then((response) => {
+    console.log(response);
+}).catch((error) => {
+    console.error('Error accessing bitcoind:', error);
+});
+
+function extractIp(address) {
+    if (address.includes('[')) {
+        return address.substring(0, address.indexOf(']') + 1);
+    }
+    return address.split(':')[0];
+}
+
+async function getGeoLocation(ip) {
+    try {
+        const cleanIp = ip.replace(/\[|\]/g, '');
+        const encodedIP = encodeURIComponent(cleanIp);
+        const url = `https://ipinfo.io/${encodedIP}?token=${ipInfoToken}`;
+        console.log("Requesting URL:", url);
+        const response = await axios.get(url);
+        console.log("API Response: ", response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error getting location:', error);
+        return null;
+    }
+}
+
+app.get('/peer-locations', async (req, res) => {
+    try {
+        const peers = await client.command('getpeerinfo');
+        const locations = await Promise.all(peers.map(async (peer) => {
+            const ip = extractIp(peer.addr);
+            const geoInfo = await getGeoLocation(ip) || {};
+            return {
+                ip: ip + '<br><span class="text-light">' + (await reverseDnsLookup(ip) + '</span>' || ''),
+                userAgent: peer.subver + '<br><span class="text-light">' + peer.version + '</span>',
+                blockHeight: peer.startingheight,
+                location: geoInfo.loc ? geoInfo.loc.split(',') : '',
+                country: (geoInfo.country && geoInfo.timezone) ? `${geoInfo.country}<br><span class="text-light">${geoInfo.timezone}</span>` : '',
+                region: geoInfo.region || '',
+                city: (geoInfo.city && geoInfo.region) ? `${geoInfo.city}<br><span class="text-light">${geoInfo.region}</span>` : '',
+                hostname: geoInfo.hostname || '',
+                org: (geoInfo.asn && geoInfo.asn.name) ? `${geoInfo.asn.name}<br><span class="text-light">${geoInfo.asn.asn}</span>` : ''
+            };
+        }));
+        res.json(locations);
+    } catch (error) {
+        console.error('Detailed error:', error);
+        res.status(500).json({ error: 'Error fetching peer locations', details: error.message });
+    }
+});
+
+async function reverseDnsLookup(ip) {
+    const dns = require('dns').promises;
+    try {
+        const hostnames = await dns.reverse(ip);
+        return hostnames[0];
+    } catch (error) {
+        return '';
+    }
+}
+
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
