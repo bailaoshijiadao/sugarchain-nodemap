51c1bc1a3753997f88a3301bca0be4f90b42cfdb
ROZ a.k.a. Aoi Emerauda <mail@mofumofu.me>
2024-04-25 18:28:16 +0900

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,26 @@
 import * as chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../app.js';
+import http from 'http';
 
 chai.use(chaiHttp);
-const { expect } = chai; // chaiからexpectを抽出
+const { expect } = chai;
+let server;
 
 describe('Node Map API', () => {
+    before(done => {
+        server = app.listen(3000, () => {
+            console.log('Server running on port 3000');
+            done();
+        });
+    });
+
+    after(done => {
+        server.close(done);
+    });
+
     it('should GET /peer-locations successfully', done => {
-        chai.request(app)
+        chai.request(server)
             .get('/peer-locations')
             .end((err, res) => {
                 expect(res).to.have.status(200);
