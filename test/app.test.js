aa906658922703243453aafaf6b8362df0ad55e6
ROZ a.k.a. Aoi Emerauda <mail@mofumofu.me>
2024-04-25 18:59:42 +0900

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,53 +1,27 @@
-async function setupChai() {
-  const chai = await import('chai');
-  const chaiHttp = await import('chai-http');
-  chai.default.use(chaiHttp.default);
-  return chai;
-}
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app.js').default;
 
-describe('Node Map API', function () {
-  let chai;
-  let expect;
+chai.use(chaiHttp);
+const { expect } = chai;
 
-  before(async function () {
-    chai = await setupChai();
-    expect = chai.default.expect;
-  });
+describe('Node Map API', () => {
+    before(async function() {
+    });
 
-  it('should GET /peer-locations successfully', function (done) {
-    chai.default.request(require('../app'))
-      .get('/peer-locations')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-});
-async function setupChai() {
-  const chaiModule = await import('chai');
-  const chaiHttpModule = await import('chai-http');
-  const chai = chaiModule.default;
-  const chaiHttp = chaiHttpModule.default;
-  
-  chai.use(chaiHttp);
-  return chai;
-}
+    after(async function() {
+    });
 
-describe('Node Map API', function () {
-  let chai;
-  let expect;
-
-  before(async function () {
-    chai = await setupChai();
-    expect = chai.expect;
-  });
-
-  it('should GET /peer-locations successfully', function (done) {
-    chai.request(app)
-      .get('/peer-locations')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
+    it('should GET /peer-locations successfully', done => {
+        chai.request(app)
+            .get('/peer-locations')
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
 });
