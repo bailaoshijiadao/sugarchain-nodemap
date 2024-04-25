21e6e7a1a3b7f52f6fa62d905d708be2f70593a2
ROZ a.k.a. Aoi Emerauda <mail@mofumofu.me>
2024-04-25 16:36:28 +0900

    Create app.test.js

---
+++ test/app.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe('Node Map API', () => {
+    it('should GET /peer-locations successfully', done => {
+        chai.request(app)
+            .get('/peer-locations')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+});
